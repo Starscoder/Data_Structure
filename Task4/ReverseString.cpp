ab1444e50e7201084682089642254931a38a0b0d
Starscoder <824048788@qq.com>
2019-08-15 20:14:38 +0800

    Create ReverseString.cpp

---
+++ Task4/ReverseString.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int j=s.size()-1;
+        for(int i=0;i<s.size()/2;i++){
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+            j--;
+        }
+    }
+};
